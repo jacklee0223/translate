b7653a2b55a9332ae3f1cb54961a6fec86d42e99
Jeong Min Lee <jacklee0223@gmail.com>
2020-02-21 00:56:52 -0600

    implement boilerplate ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
+import UserCreate from './UserCreate';
 
 class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({
+      language
+    });
+  };
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag kr"
+            onClick={() => this.onLanguageChange('korean')}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
   }
 }
 
